51c58db4fbacb56327fa944da0773a1108109928
Lerb73 <loganerb04@gmail.com>
2026-02-23 14:39:31 -0500

    Add files via upload
    Initial upload of a calculator to convert units of meters to other measurements both in metric and imperial.

---
+++ metric_converter.py
@@ -0,0 +1,23 @@
+meters = 2
+centimeters = meters*100
+milimeters = centimeters*10
+kilometers = meters*1000
+feet = meters/0.3048
+inches = feet*12
+yards = feet/3
+miles = feet/5280
+
+print "Metric:"
+print milimeters, "milimeters"
+print centimeters, "centimeters"
+print meters, "meters"
+print kilometers, "kilometers"
+
+print ""
+
+print "Imperial:"
+print inches, "inches"
+print feet, "feet"
+print yards, "yards"
+print miles, "miles"
+
